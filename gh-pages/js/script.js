4a5a25d1529dd5864bd4434718a6a3f563c0dc13
Sanadi <79256438+sanadiibnuahmad@users.noreply.github.com>
2024-07-21 01:38:16 +0700

    Create script.js

---
+++ gh-pages/js/script.js
@@ -0,0 +1,14 @@
+const flipbookContainer = document.querySelector('.flipbook-container');
+
+// Tambahkan kode Javascript untuk animasi flip dan navigasi halaman
+// (Perlu dikembangkan sesuai kebutuhan Anda)
+
+// Contoh: Membalik halaman ke halaman berikutnya
+function nextPage() {
+    // ... (kode untuk beralih ke halaman berikutnya)
+}
+
+// Contoh: Membalik halaman ke halaman sebelumnya
+function previousPage() {
+    // ... (kode untuk beralih ke halaman sebelumnya)
+}
